6a302f742f4f2e84a7ab3a2171eb4d6816792871
Haniel Ubogu <hanieltobi@gmail.com>
2023-02-24 10:03:12 +0100

    update unoconfig

--- uno.config.ts
+++ uno.config.ts
@@ -1,19 +1,12 @@
 // alteranative import
-// import {
-//   defineConfig,
-//   presetIcons,
-//   presetUno,
-//   presetAttributify,
-//   transformerDirectives,
-//   transformerVariantGroup,
-// } from "unocss";
-
-import { defineConfig } from "@unocss/vite"; // https://github.com/unocss/unocss/tree/main/packages/vite
-import presetUno from "@unocss/preset-uno"; // https://github.com/unocss/unocss/tree/main/packages/preset-uno
-import presetAttributify from "@unocss/preset-attributify"; // https://github.com/unocss/unocss/tree/main/packages/preset-attributify
-import presetIcons from "@unocss/preset-icons"; // https://github.com/unocss/unocss/tree/main/packages/preset-icons
-import transformerDirective from "@unocss/transformer-directives"; // https://github.com/unocss/unocss/tree/main/packages/transformer-directives
-import transformerVariantGroup from "@unocss/transformer-variant-group"; // https://github.com/unocss/unocss/tree/main/packages/transformer-variant-group
+import {
+  defineConfig,
+  presetIcons,
+  presetUno,
+  presetAttributify,
+  transformerDirectives,
+  transformerVariantGroup,
+} from "unocss";
 
 // https://github.com/unocss/unocss#configurations
 export default defineConfig({
@@ -29,5 +22,5 @@ export default defineConfig({
 
   // https://github.com/unocss/unocss#using-presets
   presets: [presetUno(), presetIcons({ scale: 1.2 }), presetAttributify({ prefix: "un-" })],
-  transformers: [transformerDirective(), transformerVariantGroup()],
+  transformers: [transformerDirectives(), transformerVariantGroup()],
 });
